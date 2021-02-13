38332788e828e651d697e328872ba013dbdecb53
Laura Vergara <laura.vergarar@upb.edu.co>
2021-02-13 17:47:21 -0500

    Enemy Controller fix

---
+++ Juego Taller 5/Assets/Level/Scripts/EnemyController1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController1 : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float maxRange;
+    [SerializeField]
+    private float minRange;
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerControllerTemp>().transform;
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
+        {
+            FollowPlayer();
+        }
+
+    }
+
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
