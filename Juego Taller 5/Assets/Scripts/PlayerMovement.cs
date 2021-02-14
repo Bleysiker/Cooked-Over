1660671ed941b932c09511fbfee68574de763bf4
Marcelo Alvarenga Tamayo <Marcelo.alvarenga@upb.edu.co>
2021-02-13 19:22:48 -0500

    Revert "Salud y enemigo de prueba"
    This reverts commit 243482c0c9997023331b99bc851fec324028065e.

--- Juego Taller 5/Assets/Scripts/PlayerMovement.cs
+++ Juego Taller 5/Assets/Scripts/PlayerMovement.cs
@@ -24,15 +24,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Dash();
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            HealthSystem.Instance.TakeDamage(10);
-        }
     }
 
     public void Move()
@@ -102,6 +93,4 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
-
-
 }
